2efd892c4f571fa11d31a35aabc087493003f793
ErvicLinares <ervic2015@gmail.com>
2025-02-09 17:00:58 -0500

    corriendo con vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// @ts-ignore
+import vercel from '@astrojs/vercel/server';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+
+  }
+
+});
