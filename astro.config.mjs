ff33ff0fb3be73df19511186c7a954cb19e5f88c
ErvicLinares <ervic2015@gmail.com>
2025-02-09 18:35:58 -0500

    corrigiendo ultima conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
-
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-
-  vite: {
-
-
-  }
-
+  adapter: vercel(),
 });
