af820d132df923a511dadf9c75cb1e402edc9cb7
ErvicLinares <ervic2015@gmail.com>
2025-02-09 18:49:42 -0500

    cambio static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
 });
