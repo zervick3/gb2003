72f06ecab5506c3df181dcd83fd922538d15d1f7
zervick3 <ervic2015@gmail.com>
2025-04-15 13:06:42 -0500

    mejorando y agregando pagina delay

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import vercel from '@astrojs/vercel';
 
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
   integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
